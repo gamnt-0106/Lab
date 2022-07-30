7f246a286f89fa4319f19c387d4c60515e8ef672
gamnt-0106 <gamntph14216@fpt.edu.vn>
2022-07-30 08:33:56 +0700

    cart

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -26,15 +26,15 @@ const Cart = () => {
       {cart?.map(item => (
         <div className="cart-item">
           <h4 className="min-w-[400px]">{item.name}</h4>
-          <img style={{width: "20%"}} src={item.image} alt="" />
-          <p>{item.saleOffPrice}</p>
+          <img style={{width: "20%",height: "20%"}} src={item.image} alt="" />
           <div className="flex justify-center w-1/5">
-            <svg onClick={() => downCount(item)} className="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" /></svg>
+          <button onClick={() => downCount(item)}>-</button>
             <input value={item?.count ? item?.count : 1} className="mx-2 text-center w-12" type="text" defaultValue="1"/>
-            <svg onClick={() => upCount(item)} className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-              <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-            </svg>
+            
+            <button onClick={() => upCount(item)}>+</button>
           </div>
+          <p>{total}</p>
+
         </div>
       ))}
       <div className="total">
